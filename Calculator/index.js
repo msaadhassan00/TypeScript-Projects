e63d2625e028f3c7bcff05e29d8aef17b4addc7d
msaadhassan00@ <msaadhassan00@gmail.com>
2022-10-03 00:15:25 +0500

    Calculator Project

---
+++ Calculator/index.js
@@ -0,0 +1,48 @@
+"use strict";
+exports.__esModule = true;
+var readline_sync_1 = require("readline-sync");
+function main() {
+    var firstStr = (0, readline_sync_1.question)("Enter First Number: ");
+    var operator = (0, readline_sync_1.question)("Enter Operator: ");
+    var secondStr = (0, readline_sync_1.question)("Enter Second Number: ");
+    var validInput = isNumber(firstStr) && isOperator(operator) && isNumber(secondStr);
+    if (validInput) {
+        var firstNum = parseInt(firstStr);
+        var secondNum = parseInt(secondStr);
+        var result = calculate(firstNum, operator, secondNum);
+        console.log(result);
+    }
+    else {
+        console.log('Invalid Input');
+        main();
+    }
+}
+function calculate(firstNum, operator, secondNum) {
+    switch (operator) {
+        case '+':
+            return firstNum + secondNum;
+        case '-':
+            return firstNum - secondNum;
+        case '*':
+            return firstNum * secondNum;
+        case '/':
+            return firstNum / secondNum;
+    }
+}
+function isOperator(operator) {
+    switch (operator) {
+        case "+":
+        case "-":
+        case "*":
+        case "/":
+            return true;
+        default:
+            return false;
+    }
+}
+function isNumber(str) {
+    var maybeNum = parseInt(str);
+    var isNum = !isNaN(maybeNum);
+    return isNum;
+}
+main();
